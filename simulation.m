c3fb41994c775368d6bd664fe7d58ce4ef598ac1
hoangnguyen <hsnguyen@kth.se>
2014-07-11 17:27:04 +0100

    update script, data and README

--- simulation.m
+++ simulation.m
@@ -1,15 +1,9 @@
 
 function simulation(md,env,task)
     gr_start=1;
-    if md==3
-        gr_end=6;
-        envname={'ishii0.1C' 'ishii0.4C' 'ishii0.7C' 'holm0.67B' 'perrenoud0.65B' 'emmerling0.09N'};
-        dilution=[0.1 0.4 0.7 0.67 0.65 0.09];
-    elseif md==1 || md==2
-        gr_end=12;
-        envname={'nanchen0.09C' 'nanchen0.4C' 'emmerling0.09N' 'emmerling0.09C' 'emmerling0.4C' 'yang0.1C' 'yang0.55C' 'ishii0.1C' 'ishii0.4C' 'ishii0.7C' 'perrenoud0.65B' 'holm0.67B'};
-        dilution=[0.09 0.4 0.09 0.09 0.4 0.1 0.55 0.1 0.4 0.7 0.65 0.67];
-    end
+    gr_end=6;
+    envname={'ishii0.1C' 'ishii0.4C' 'ishii0.7C' 'holm0.67B' 'perrenoud0.65B' 'emmerling0.09N'};
+    dilution=[0.1 0.4 0.7 0.67 0.65 0.09];
     if ~exist('md','var')
         md=1;
     end
@@ -29,23 +23,16 @@ function simulation(md,env,task)
     %%% Parsing input parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% md:  1-default. E coli core model
     %           2-Schuetz model
-    %           3-Genome scale model iAF1260
     %%% env: experimental data, if not specified, all are taken
-    %%% task:   1-default. Use glucose uptake+additional constraints.
-    %       2-pairs of those constraints.
-    %		3-run all linear objective function and plot fidelity points (not bars).
-    %       4-scatter plot
-    %       5-run multiple objective functions optimization with 2
-    %       objectives: MaxBM and other linear objectives.
-    %       6-run multiple objective functions optimization with 3
-    %       objectives: MaxBM, MaxATP, and other linear objectives.
-    %       7-run multiple objective functions optimization with 3
-    %       objectives: MaxBM, Min Flux, and other linear objectives.
+    %%% task:   1-default. Use glucose uptake+additional constraints for FBAs.
+    %       2-FBAs using pairwise constraints.
+    %		3-FBAs with all linear objective functions.
+    %       4-Scatter plot
     mdname={'ec_core_model' 'ec_schuetz_model' 'ec_iaf1260_model'};
     
-    taskname={'glucose+coarse_grained' 'pairsOfConstraint' 'allLinearOFs' 'scatterPlot' 'biomass+X' 'biomass+ATP+X''biomass+minFlux+X'};    
+    taskname={'glucose+coarse_grained' 'pairsOfConstraint' 'allLinearOFs' 'scatterPlot'};    
     load_model=strcat('models/',mdname{md});  
-    load_data=strcat('data/',mdname{md},'.mat');
+    load_data=strcat('data/Expdata_',mdname{md},'.mat');
     
     glob.ite=100;
     tit={'max BM','max ATP','min Flux','max BM/flux','max ATP/flux','min Rd','min ATPprod','max ATPprod'};
@@ -72,8 +59,6 @@ function simulation(md,env,task)
             multiplier=strainM(13,gr)/bmrate;
         elseif md==2
             multiplier=strainM(98,gr)/bmrate;
-        elseif md==3
-            multiplier=strainM(1005,gr)/bmrate;
         end
     
 %         log(:,1+(1+noOfObj)*(gr-1))=[glob.exp_flux;dilution(gr);-consump_rate(1);-consump_rate(2)];
@@ -84,7 +69,7 @@ function simulation(md,env,task)
             model.ub(model.ub~=10000)=model.ub(model.ub~=10000)*multiplier;
             model.lb(model.lb~=-10000)=model.lb(model.lb~=-10000)*multiplier;
         end
-        if md==1 || md==3
+        if md==1
             % No more additional constraints
             model=changeRxnBounds(model,'ATPM',0,'l');
             model=changeRxnBounds(model,'EX_glc(e)',-100,'b');
@@ -252,172 +237,8 @@ function simulation(md,env,task)
             [solution,id]=sort(solution);
             tit=tit(id);
         elseif task==4       
-
             solution=allObjCall(glob,model,md,task);
-        elseif task==5
-            bm={'Biomass_Ecoli_core_w_GAM' 'biomass' 'Ec_biomass_iAF1260_core_59p81M'};
-            % range of biomass flux
-            model = changeObjective(model,bm{md});
-            fba= optimizeCbModel(model,'min');                
-            bmMin= fba.f;
-            fba= optimizeCbModel(model,'max');                
-            bmMax= fba.f;
             
-            tit={};
-            solution=[];
-            for i=1:length(model.mets)
-                if (md==1 || md==3) && ~isempty(strfind(model.mets{i},'[e]'))  
-                    continue;
-                end
-                if md==2 && i>48
-                    break
-                end
-                if ~isempty(strfind(lower(model.mets{i}),'atp'))
-                    continue;
-                end
-                amodel=model;
-                c=amodel.S(i,:);
-                amodel.csense(i)='G';
-                amodel.c=full(c');
-                
-                mindist=pareto2(glob,amodel,@maxX,md,task,bmMin,bmMax);              
-                tit=[tit strcat('+Max',{' '},model.mets{i})];
-                solution=[solution;mindist];
-%                 fprintf('model: %s, dataset: %s, metabolite: %d/%d\n',mdname{md},envname{gr},i,length(model.mets));
-            end
-            
-            mindist=pareto2(glob,amodel,@minRedox,md,task,bmMin,bmMax);
-            solution=[solution;mindist];
-            tit=[tit '+Min Rd'];
-
-            mindist=pareto2(glob,amodel,@minATPProd,md,task,bmMin,bmMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Min ATPprod'];
-
-            mindist=pareto2(glob,amodel,@maxATPProd,md,task,bmMin,bmMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Max ATPprod'];
-
-            mindist=pareto2(glob,amodel,@minManhattan,md,task,bmMin,bmMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Min Flux']; 
-            
-            tit=tit(solution>0);
-            solution=solution(solution>0);
-
-            [solution,id]=sort(solution);
-            tit=tit(id);
-	    
-        elseif task==6
-            bm={'Biomass_Ecoli_core_w_GAM' 'biomass' 'Ec_biomass_iAF1260_core_59p81M'};
-            atpm={'ATPM' 'maint' 'ATPM'};
-            % range of biomass flux
-            model = changeObjective(model,bm{md});
-            fba= optimizeCbModel(model,'min');                
-            bmMin= fba.f;
-            fba= optimizeCbModel(model,'max');                
-            bmMax= fba.f;
-            % range of ATPM 
-            model = changeObjective(model,atpm{md});
-            fba= optimizeCbModel(model,'min');                
-            atpMin= fba.f;
-            fba= optimizeCbModel(model,'max');                
-            atpMax= fba.f;
-            tit={};
-            solution=[];
-            for i=1:length(model.mets)
-                if (md==1 || md==3) && ~isempty(strfind(model.mets{i},'[e]'))  
-                    continue;
-                end
-                if md==2 && i>48
-                    break
-                end
-                if ~isempty(strfind(lower(model.mets{i}),'atp'))
-                    continue;
-                end
-                amodel=model;
-                c=amodel.S(i,:);
-                amodel.csense(i)='G';
-                amodel.c=full(c');
-                
-                mindist=pareto3(glob,amodel,@maxX,md,task,bmMin,bmMax,atpMin,atpMax);              
-                tit=[tit strcat('+Max',{' '},model.mets{i})];
-                solution=[solution;mindist];
-%                 fprintf('model: %s, dataset: %s, metabolite: %d/%d\n',mdname{md},envname{gr},i,length(model.mets));
-            end
-            
-            mindist=pareto3(glob,amodel,@minRedox,md,task,bmMin,bmMax,atpMin,atpMax);
-            solution=[solution;mindist];
-            tit=[tit '+Min Rd'];
-
-            mindist=pareto3(glob,amodel,@minATPProd,md,task,bmMin,bmMax,atpMin,atpMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Min ATPprod'];
-
-            mindist=pareto3(glob,amodel,@maxATPProd,md,task,bmMin,bmMax,atpMin,atpMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Max ATPprod'];
-
-            mindist=pareto3(glob,amodel,@minManhattan,md,task,bmMin,bmMax,atpMin,atpMax);    
-            solution=[solution;mindist];
-            tit=[tit '+Min Flux']; 
-            
-            tit=tit(solution>0);
-            solution=solution(solution>0);
-
-            [solution,id]=sort(solution);
-            tit=tit(id);
-	    
-           
-        
-        elseif task==7
-            bm={'Biomass_Ecoli_core_w_GAM' 'biomass' 'Ec_biomass_iAF1260_core_59p81M'};
-            % range of biomass flux
-            model = changeObjective(model,bm{md});
-            fba= optimizeCbModel(model,'min');                
-            bmMin= fba.f;
-            fba= optimizeCbModel(model,'max');                
-            bmMax= fba.f;
-
-            tit={};
-            solution=[];
-            for i=1:length(model.mets)
-                if (md==1 || md==3) && ~isempty(strfind(model.mets{i},'[e]'))  
-                    continue;
-                end
-                if md==2 && i>48
-                    break
-                end
-                if ~isempty(strfind(lower(model.mets{i}),'atp'))
-                    continue;
-                end
-                amodel=model;
-                c=amodel.S(i,:);
-                amodel.csense(i)='G';
-                amodel.c=full(c');
-                max_sol=optimizeCbModel(model,'max');
-                min_sol=optimizeCbModel(model,'min');
-                min_X=0;
-                max_X=10000;
-                if max_sol.stat~=1
-                    max_X=max_sol.f;
-                end
-                if min_sol.stat~=1
-                    min_X=min_sol.f;
-                end
-                                
-                mindist=pareto3_2(glob,amodel,i,md,task,bmMin,bmMax,min_X,max_X);              
-                tit=[tit strcat('+Max',{' '},model.mets{i})];
-                solution=[solution;mindist];
-%                 fprintf('model: %s, dataset: %s, metabolite: %d/%d\n',mdname{md},envname{gr},i,length(model.mets));
-            end
-            
-            tit=tit(solution>0);
-            solution=solution(solution>0);
-
-            [solution,id]=sort(solution);
-            tit=tit(id);
-	    
         end
     
         fname=strcat(savedir,'/',taskname{task});
@@ -425,12 +246,5 @@ function simulation(md,env,task)
         fprintf('----------FINISH TASK %d:  %s, %s----------\n',task,mdname{md},envname{gr});
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-%         for k=1:noOfObj
-%             log(:,1+(noOfObj+1)*(gr-1)+k)=solution{1}{1,k}.xmin;
-%         end
-
-
     end
-%    matlabpool close;
-%     dlmwrite(strcat('results',mdname{md},'\','tmp.log'),log);
  end
