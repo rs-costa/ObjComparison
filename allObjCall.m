24b1813fd97ef7e7deec9ad51c8c66120f515a22
Rafael Costa <r-costa@users.noreply.github.com>
2015-03-10 23:21:49 +0000

    Update allObjCall.m

--- allObjCall.m
+++ allObjCall.m
@@ -1,3 +1,11 @@
+% Exploring the cellular objective in flux balance constraint-based models.
+% Author: Rafael Costa
+% Affiliation: Instituto de Engenharia de Sistemas e Computadores, Investigacão e Desenvolvimento (INESC-ID)
+% and
+% Center for Intelligent Systems, LAETA, IDMEC, IST, University of Lisbon 
+% Author: Son Nguyen
+
+
 function solution=allObjCall(glob,model,netcode,taskcode)
 %%% Objective functions for FBA %%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
